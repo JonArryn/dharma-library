0c8408918bdc16168107ac208f28f37142a2af6e
JonArryn <dumpster@lordofthevale.com>
2023-06-23 18:07:13 -0400

    Begin library page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import 'App.css';
-import Typography from '@mui/material/Typography';
 import GlobalsProvider from 'provider/GlobalsProvider';
 import HomeTopNav from 'component/navigation/HomeTopNav';
 import { Outlet } from 'react-router-dom';
